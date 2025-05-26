3b7d7d3170f30e0d19e8f82abac0d232a8cfce74
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2025-05-25 23:00:31 -0700

    read config file, find download file, implement 3 way handshake

--- transport-layer/tcpserver.py
+++ transport-layer/tcpserver.py
@@ -11,7 +11,13 @@ TIMEOUT = 0.5   # timeout time
 
 class Server():
     def __init__(self, config_file):
-        #Read the config file and initialize the port, peer_num, peer_info, content_info from the config file
+        # Read the config file and initialize the port, peer_num, peer_info, content_info from the config file
+        config = json.load(open(config_file, "r"))
+        self.hostname = config["hostname"]
+        self.port = config["port"]
+        self.peers = config["peers"]
+        self.content_info = config["content_info"] # list of filenames
+        self.peer_info = config["peer_info"]  # list of peer info, each peer info is a dict with hostname, port, content info
 
         # establish a socket according to the information
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #NOTE THAT THE SOCK_DGRAM will ensure your socket is UDP
@@ -23,26 +29,41 @@ class Server():
         self.remain_threads = True
         self.cli()
         return
-    
-    def find_file(self, file_name):
-        #A function to find the peer with the file you want!
 
-    
+    def find_file(self, file_name):
+        # returns a tuple of (hostname, port) of the peer that has the file
+        for peer in self.peer_info:
+            if file_name in peer["content_info"]:
+                return (peer["hostname"], peer["port"])
+
     def load_file(self, file_name):
         # find which server has the file
+        addr = self.find_file(file_name) # addr is a tuple (hostname, port)
+
         # establish a client socket for downloading file
         self.cl_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-        
+
         # use a connect flag to determine if the file name is sent correctly
-        
-        #Initiate three-way handshake and use a connect flag
-        
+        connect_flag = False
+
+        # Initiate three-way handshake and use a connect flag
         while not connect_flag:
             try:
                 # handshake
+                initial_message = json.dumps({"type": "SYN", "file_name": file_name}).encode()
+                self.cl_socket.sendto(initial_message, addr)
+                data, _ = self.cl_socket.recvfrom(BUFSIZE)
+                response = json.loads(data.decode())
+
+                if response.get("type") == "SYN-ACK":
+                    connect_flag = True
+                    # send ACK
+                    ack_message = json.dumps({"type": "ACK"}).encode()
+                    self.cl_socket.sendto(ack_message, addr)
             except socket.timeout:
                 # handshake failed
-      
+                continue
+
         # the receiver keeps a record for which part has been acked
 
         # start receiving file
@@ -85,22 +106,33 @@ class Server():
         #When done transmitting, close the threads.
 
     def listener(self): # listen to the socket to see if there's any transmission request
-        #Do any initializations that you want
         while self.remain_threads:
-            file_name = ""
             try:
-                file_name, addr = self.server_socket.recvfrom(BUFSIZE)
-                #Receive the file name and requesting address from the UDP
+                data, addr = self.server_socket.recvfrom(BUFSIZE)
+                message = json.loads(data.decode())
+
+                if message.get("type") == "SYN":
+                    requested_file = message.get("file_name")
+
+                    syn_ack_message = json.dumps({"type": "SYN-ACK"}).encode()
+                    self.server_socket.sendto(syn_ack_message, addr)
+
+                    # start transmission after receiving ACK
+                    try:
+                        ack_data, ack_addr = self.server_socket.recvfrom(BUFSIZE)
+                        ack_message = json.loads(ack_data.decode())
+                        if ack_message.get("type") == "ACK":
+                            transmission_thread = threading.Thread(target=self.transmit, args=(requested_file, addr))
+                            transmission_thread.start()
+
+                    # ACK not received, continue waiting for ACK
+                    except socket.timeout:
+                        continue
+
+            # no data received, continue listening
             except socket.timeout:
-                pass
-            
-            if file_name == "":
-                pass
-            else:   # start transmission
-                ;#Create a transmit thread (HINT : you can have a large array of transmit threads if you want) and start it
-                
-        return
-    
+                continue
+
     def cli(self):  # cli interface for input of the file name
         listen_thread = threading.Thread(target=self.listener)
         listen_thread.start()
