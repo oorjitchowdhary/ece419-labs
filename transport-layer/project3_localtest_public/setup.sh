77c8a9ea39237ffcf3111a4f70f5643725dbe040
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2025-05-26 23:56:17 -0700

    verify lossless cmu.jpg transfer, add Dockerfile for testing

--- transport-layer/project3_localtest_public/setup.sh
+++ transport-layer/project3_localtest_public/setup.sh
@@ -3,4 +3,4 @@ sudo apt update
 sudo apt install python3 iptables python3-pip python3-dev python3-
 sudo apt install libnfnetlink-dev
 sudo apt install libnetfilter-queue-dev
-pip3 install netfilterqueue
+pip3 install nfqp3
